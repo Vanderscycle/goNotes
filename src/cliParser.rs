13b196701515fbb8d863f65f89c2d15e05458985
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-02 09:23:42 -0800

    playing with rust

---
+++ src/cliParser.rs
@@ -0,0 +1,3 @@
+pub fn parser(arg: &str) -> String {
+    format!("Received message: {}", arg)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[allow(non_snake_case)]
+mod cliParser;
 fn main() {
-    println!("Hello, world!");
+    let receivedMsg: String = cliParser::parser("Hello, world!");
+    println!("{}", &receivedMsg);
+    println!(
+        "Event simpler method {}",
+        cliParser::parser("STRAIGHT FROM FN")
+    );
 }
