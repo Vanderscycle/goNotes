75dafc07809b272ed3d2f3c866010ba9469e3ce8
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-17 14:30:56 -0700

    moved flags arguments to a separate package

--- main.go
+++ main.go
@@ -1,9 +1,106 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"goNotes/flags"
+	"os"
 )
+
+type model struct {
+	choices  []string         // items on the to-do list
+	cursor   int              // which to-do list item our cursor is pointing at
+	selected map[int]struct{} // which to-do items are selected
+}
+
+func initialModel() model {
+	return model{
+		// Our shopping list is a grocery list
+		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+
+		// A map which indicates which choices are selected. We're using
+		// the  map like a mathematical set. The keys refer to the indexes
+		// of the `choices` slice, above.
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	// The header
+	s := "What should we buy at the market?\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
+}
 
 //Go’s structs are typed collections of fields. They’re useful for grouping data together to form records.
 //Go does not have optional type (remember that go a declared var must be used)
@@ -13,21 +110,10 @@ type Entry struct {
 }
 
 func main() {
-	num := flag.Int("n", 0, "# of iteration")
-	entry := flag.String("t", "empty", "New task")
-	flag.Parse()
-
-	n := *num
-	//prints the type
-	fmt.Printf("var type: %T\n", n)
-
-	for i := 0; i < n; i++ {
-		// 1:=0
-		// for i < n {i++}
-		fmt.Printf("hello")
+	flags.Flags()
+	p := tea.NewProgram(initialModel())
+	if err := p.Start(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
 	}
-	if *entry != "empty" {
-		fmt.Printf("%v\n", *entry)
-	}
-
 }
