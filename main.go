86b167f232a45a2493bee34b774778de93e1e8a5
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-18 15:34:03 -0700

    ok I am starting to graps what is going on  :)

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ import (
 	// "os"
 	// "strings"
 	// "github.com/charmbracelet/bubbles/textinput"
-	"github.com/charmbracelet/bubbles/key"
 	// "github.com/charmbracelet/bubbles/progress"
+	// "github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
-	// "github.com/charmbracelet/lipgloss"
 	"goNotes/keymaps"
 )
 
@@ -43,7 +43,7 @@ func initialModel() model {
 	s := spinner.New()
 	s.Spinner = spinner.Dot
 
-	return model{spinner: s}
+	return model{spinner: s, keymap: keymaps.DefaultKeyMap}
 }
 
 func (m model) Init() tea.Cmd {
@@ -57,11 +57,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch {
 		case key.Matches(msg, m.keymap.Quit):
-			fmt.Printf("%s", msg)
+			msg2 := "quit"
+			fmt.Println("%s", msg2)
 			m.quitting = true
+			os.Exit(1)
 			return m, tea.Quit
 
+		case key.Matches(msg, m.keymap.Down):
+			fmt.Printf("Down")
+			return m, nil
+
+		case key.Matches(msg, m.keymap.Up):
+			fmt.Printf("up")
+			return m, nil
+
 		default:
+			// fmt.Printf("%s && %v", msg, key.Matches(msg, m.keymap.Quit))
 			return m, nil
 		}
 
@@ -74,14 +85,13 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.spinner, cmd = m.spinner.Update(msg)
 		return m, cmd
 	}
-
 }
 
 func (m model) View() string {
 	if m.err != nil {
 		return m.err.Error()
 	}
-	str := fmt.Sprintf("\n%s\n   %s Loading forever...press q to quit\n\n", "hello", m.spinner.View())
+	str := fmt.Sprintf("\n\n   %s Loading forever...press q to quit\n\n", m.spinner.View())
 	if m.quitting {
 		return str + "\n"
 	}
@@ -89,6 +99,7 @@ func (m model) View() string {
 }
 
 func main() {
+
 	fmt.Printf("hello %s", "test")
 	p := tea.NewProgram(initialModel())
 	if err := p.Start(); err != nil {
