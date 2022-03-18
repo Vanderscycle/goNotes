85d85b87f3731759e5b04097b389556c08af3e53
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-18 14:33:27 -0700

    more learning of go using bubbles

--- firstTui.lessons.md
+++ firstTui.lessons.md
@@ -8,3 +8,24 @@ What happens within the Elm program though? It always breaks into three parts:
     * View — a way to turn your state into HTML
     * Update — a way to update your state based on messages
 
+## Golang learning
+
+### Golang pointers
+background [info](https://gobyexample.com/pointers) about pointers
+
+### Value receiver vs. pointer receiver
+background [ info ](https://gobyexample.com/methods) about methods about methods
+Go automatically handles conversion between values and pointers for method calls.
+
+```go
+type T struct {
+    a int
+}
+func (tv  T) Mv(a int) int         { return 0 }  // value receiver
+func (tp *T) Mp(f float32) float32 { return 1 }  // pointer receiver
+```
+
+### closures (lambda functions)
+background [info](https://gobyexample.com/closures) about closures
+
+
--- main.go
+++ main.go
@@ -5,31 +5,87 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
+
 	// "os"
 	// "strings"
 	// "github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/progress"
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	// "github.com/charmbracelet/lipgloss"
 )
 
+const (
+	padding  = 2
+	maxWidth = 80
+)
+
+type errMsg error
+
+type tickMsg time.Time
 type model struct {
 	err      error
+	progress progress.Model
 	spinner  spinner.Model
 	quitting bool
 }
 
 func initialModel() model {
+	//progess bar
+
+	//spinner
 	s := spinner.New()
 	s.Spinner = spinner.Dot
 	return model{spinner: s}
 }
 
-func (m *model) Init() tea.Cmd {
+func (m model) Init() tea.Cmd {
 	// Just return `nil`, which means "no I/O right now, please."
-	return nil
+	return m.spinner.Tick
 }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+		default:
+			return m, nil
+		}
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+
+	default:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+
+}
+
+func (m model) View() string {
+	if m.err != nil {
+		return m.err.Error()
+	}
+	str := fmt.Sprintf("\n%s\n   %s Loading forever...press q to quit\n\n", "hello", m.spinner.View())
+	if m.quitting {
+		return str + "\n"
+	}
+	return str
+}
+
 func main() {
 	fmt.Printf("hello %s", "test")
-	// tea.NewProgram(model tea.Model, opts ...tea.ProgramOption)
+	p := tea.NewProgram(initialModel())
+	if err := p.Start(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
